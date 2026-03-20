75754d66832e5bb7ce1c2bb712b50cbd0b338787
midboob <edwarddan72@gmail.com>
2026-03-19 20:55:39 -0700

    feat: ✨ changes

--- configs/home-manager/modules/theme/stylix.nix
+++ configs/home-manager/modules/theme/stylix.nix
@@ -23,45 +23,45 @@
     # 	base0F = "d65d0e";
     # };
 
-    # # Rose pine
-    # base16Scheme = {
-    # 	base00 = "191724";
-    # 	base01 = "1f1d2e";
-    # 	base02 = "26233a";
-    # 	base03 = "6e6a86";
-    # 	base04 = "908caa";
-    # 	base05 = "e0def4";
-    # 	base06 = "e0def4";
-    # 	base07 = "524f67";
-    # 	base08 = "eb6f92";
-    # 	base09 = "f6c177";
-    # 	base0A = "ebbcba";
-    # 	base0B = "31748f";
-    # 	base0C = "9ccfd8";
-    # 	base0D = "c4a7e7";
-    # 	base0E = "f6c177";
-    # 	base0F = "524f67";
-    # };
-
-    # Catpuccin-mocha
+    # Rose pine
     base16Scheme = {
-      base00 = "1e1e2e"; # base
-      base01 = "181825"; # mantle
-      base02 = "313244"; # surface0
-      base03 = "45475a"; # surface1
-      base04 = "585b70"; # surface2
-      base05 = "cdd6f4"; # text
-      base06 = "f5e0dc"; # rosewater
-      base07 = "b4befe"; # lavender
-      base08 = "f38ba8"; # red
-      base09 = "fab387"; # peach
-      base0A = "f9e2af"; # yellow
-      base0B = "a6e3a1"; # green
-      base0C = "94e2d5"; # teal
-      base0D = "89b4fa"; # blue
-      base0E = "cba6f7"; # mauve
-      base0F = "f2cdcd"; # flamingo
+    	base00 = "191724";
+    	base01 = "1f1d2e";
+    	base02 = "26233a";
+    	base03 = "6e6a86";
+    	base04 = "908caa";
+    	base05 = "e0def4";
+    	base06 = "e0def4";
+    	base07 = "524f67";
+    	base08 = "eb6f92";
+    	base09 = "f6c177";
+    	base0A = "ebbcba";
+    	base0B = "31748f";
+    	base0C = "9ccfd8";
+    	base0D = "c4a7e7";
+    	base0E = "f6c177";
+    	base0F = "524f67";
     };
+
+    # # Catpuccin-mocha
+    # base16Scheme = {
+    #   base00 = "1e1e2e"; # base
+    #   base01 = "181825"; # mantle
+    #   base02 = "313244"; # surface0
+    #   base03 = "45475a"; # surface1
+    #   base04 = "585b70"; # surface2
+    #   base05 = "cdd6f4"; # text
+    #   base06 = "f5e0dc"; # rosewater
+    #   base07 = "b4befe"; # lavender
+    #   base08 = "f38ba8"; # red
+    #   base09 = "fab387"; # peach
+    #   base0A = "f9e2af"; # yellow
+    #   base0B = "a6e3a1"; # green
+    #   base0C = "94e2d5"; # teal
+    #   base0D = "89b4fa"; # blue
+    #   base0E = "cba6f7"; # mauve
+    #   base0F = "f2cdcd"; # flamingo
+    # };
 
     # # Stella
     # base16Scheme = {
