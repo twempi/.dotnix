e3cc8d50cbd47ebc76811c7e193e4e315bf673c1
midboob <edwarddan72@gmail.com>
2026-04-04 12:06:51 -0700

    feat: ✨ no need

--- configs/hosts/desktop/system/modules.nix
+++ configs/hosts/desktop/system/modules.nix
@@ -2,11 +2,11 @@
   imports = [
     ./modules/aoc-q27g3xmn.nix
     ./modules/boot.nix
-    ./modules/drives.nix
+    # ./modules/drives.nix
     ./modules/graphics.nix
     # ./modules/optimizations.nix
-    ./modules/packages.nix
-    ./modules/polkit.nix
-    ./modules/services.nix
+    # ./modules/packages.nix
+    # ./modules/polkit.nix
+    # ./modules/services.nix
   ];
 }
--- configs/hosts/desktop/system/modules/boot.nix
+++ configs/hosts/desktop/system/modules/boot.nix
@@ -1,14 +1,21 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
-  # Boot settings
-  boot = {
-    loader = {
-      grub = {
-      };
+  boot.loader.systemd-boot.enable = false;
+
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-uuid/B09A-1B30";
+    fsType = "vfat";
+  };
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = true;
     };
   };
-  boot.supportedFilesystems = ["ntfs"];
 }
