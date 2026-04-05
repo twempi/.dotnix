7911a176b71f9528eccf74b7e8b2ba3105f816af
midboob <edwarddan72@gmail.com>
2026-04-05 15:00:21 -0700

    feat: ✨ added system specific syncthing config

--- configs/home-manager/modules/syncthing/default.nix
+++ configs/home-manager/modules/syncthing/default.nix
@@ -1,28 +1,22 @@
 {
   services.syncthing = {
     enable = true;
+    overrideFolders = true;
+    overrideDevices = true;
 
     guiAddress = "127.0.0.1:8384";
 
     settings = {
       devices = {
-        desktop = {
-          id = "ZMFIETG-MG7WV4P-2V6RHY4-JKDNXHB-X3INDKM-TSRFAVX-DARM2ZJ-XEBPQAZ";
-        };
-
-        t480s = {
-          id = "QN7LOTX-BA4PZHR-CXZ47JH-POMXASY-6T4JPEW-3XEXAMH-77K4ACA-DONSCAX";
-        };
-
-        g14 = {
-          id = "AN7LFNP-RPK5BQO-77CE7SD-QQNPBQF-CLXRNSG-INK6UB5-QTZUPEY-AVKNAAC";
-        };
+        desktop.id = "ZMFIETG-MG7WV4P-2V6RHY4-JKDNXHB-X3INDKM-TSRFAVX-DARM2ZJ-XEBPQAZ";
+        t480s.id = "QN7LOTX-BA4PZHR-CXZ47JH-POMXASY-6T4JPEW-3XEXAMH-77K4ACA-DONSCAX";
+        g14.id = "AN7LFNP-RPK5BQO-77CE7SD-QQNPBQF-CLXRNSG-INK6UB5-QTZUPEY-AVKNAAC";
       };
 
       options = {
-        localAnnounceEnabled = false; # eduroam blocks this anyway
+        localAnnounceEnabled = false;
         natEnabled = true;
-        relaysEnabled = true; # important one
+        relaysEnabled = true;
         globalAnnounceEnabled = true;
         startBrowser = false;
         urAccepted = -1;
@@ -31,12 +25,13 @@
       folders = {
         notes = {
           id = "notes";
-          devices = ["desktop" "t480s" "g14"];
+          devices = [ "desktop" "t480s" "g14" ];
           path = "/home/edward/Documents/notes";
         };
+
         school = {
           id = "school";
-          devices = ["desktop" "t480s" "g14"];
+          devices = [ "desktop" "t480s" "g14" ];
           path = "/home/edward/Documents/school";
         };
       };
---
+++ configs/hosts/desktop/home/modules/syncthing/default.nix
@@ -0,0 +1,6 @@
+{
+  services.syncthing.settings.folders = {
+    notes.path = "/mnt/Storage/Documents/notes";
+    school.path = "/mnt/Storage/Documents/school";
+  };
+}
