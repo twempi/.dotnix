dc769a701f3c3a56859dd4611716ff1cf5a0af43
midboob <edwarddan72@gmail.com>
2026-04-08 16:26:27 -0700

    feat: ✨ changes

--- configs/hosts/desktop/system/modules/drives.nix
+++ configs/hosts/desktop/system/modules/drives.nix
@@ -1,20 +1,30 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
   # Mounting additional drives
   fileSystems = {
     "/mnt/Storage" = {
       device = "/dev/disk/by-uuid/9A62A5A562A5871B";
       fsType = "ntfs-3g";
-      options = ["rw" "uid=1000" "gid=100" "dmask=022" "fmask=133" "exec" "nofail"];
+      options = [
+        "rw"
+        "uid=1000"
+        "gid=100"
+        "umask=000"
+        "exec"
+        "nofail"
+      ];
     };
 
     "/mnt/Windows" = {
       device = "/dev/disk/by-uuid/01DC4262B6A13A40";
       fsType = "ntfs-3g";
-      options = ["rw" "uid=1000" "nofail" "gid=100" "dmask=022" "fmask=133"];
+      options = [
+        "rw"
+        "uid=1000"
+        "gid=100"
+        "umask=000"
+        "exec"
+        "nofail"
+      ];
     };
   };
 }
--- configs/hosts/t480s/system/modules/minecraft.nix
+++ configs/hosts/t480s/system/modules/minecraft.nix
@@ -59,7 +59,7 @@
         serverProperties = {
           server-port = 25565;
           difficulty = 3;
-          gamemode = 1;
+          gamemode = 0;
           max-players = 20;
           motd = "NASA Server";
           spawn-protection = 0;
