6519473695c657cb6a224f8898115f4810e92f6d
edward <113312727+twempi@users.noreply.github.com>
2026-04-14 15:14:29 -0700

    Update login-manager.nix
    can't login on g14 so removing these changes

--- configs/system/modules/login-manager.nix
+++ configs/system/modules/login-manager.nix
@@ -5,26 +5,6 @@
       save = true;
       load = true;
 
-      # UI
-      clock = "%a %b %d  %H:%M";
-      bigclock = "en";
-      bigclock_seconds = true;
-      hide_borders = false;
-      box_title = "Welcome";
-      default_input = "login";
-
-      # Colors / layout
-      bg = "0x00000000";
-      border_fg = "0x00FFFFFF";
-      blank_box = true;
-      hide_key_hints = false;
-      hide_version_string = false;
-      show_tty = true;
-
-      # Sessions / behavior
-      shell = false;
-      vi_mode = true;
-
       # Animation
       animation = "matrix";
       animation_timeout_sec = 0;
