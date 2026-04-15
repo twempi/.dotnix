cc12db7eb7265a2c748bae6bc619fe9aeb983a0b
midboob <edwarddan72@gmail.com>
2026-04-15 13:34:25 -0700

    feat: ✨ added plymouth theme and changed stylix theme

--- configs/system/modules/bootloader.nix
+++ configs/system/modules/bootloader.nix
@@ -1,4 +1,12 @@
-{
+{pkgs, ...}: let
+  mac-style-src = pkgs.fetchFromGitHub {
+    owner = "SergioRibera";
+    repo = "s4rchiso-plymouth-theme";
+    rev = "2f782f4b68ce1c00cef3fde6970d7b4241bb97d4";
+    sha256 = "sha256-bjtQvzupAFX5AYAIyBXSFgWhaG4nP4TvgKDoKyUhZ4U=";
+  };
+  mac-style-load = pkgs.callPackage "${mac-style-src}/package.nix" {};
+in {
   stylix.targets.grub = {
     enable = true;
   };
@@ -12,6 +20,21 @@
         efiSupport = true;
         useOSProber = true;
       };
+      timeout = 5;
     };
+
+    plymouth = {
+      enable = true;
+      theme = "mac-style";
+      themePackages = [mac-style-load];
+    };
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
   };
 }
