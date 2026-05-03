5fe4a2aa18908354b6771299bfbcbb7ce219c916
midboob <edwarddan72@gmail.com>
2026-05-03 16:46:58 -0700

    feat: ✨ suwayomi changes

--- configs/hosts/t480s/system/modules/suwayomi.nix
+++ configs/hosts/t480s/system/modules/suwayomi.nix
@@ -9,11 +9,9 @@
         "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json"
       ];
 
-      # Optional: download chapters as CBZ files.
       downloadAsCbz = true;
 
-      # Optional: local source folder.
-      # You can put local manga files here later if you want.
+      # Local source folder.
       # localSourcePath = "/var/lib/suwayomi-server/local";
     };
   };
