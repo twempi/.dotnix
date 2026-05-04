afdf549e717c606dcd3d6c0c0ac9d1b996534228
midboob <edwarddan72@gmail.com>
2026-05-04 05:14:53 -0700

    feat: ✨ removed quickshell bloat

--- configs/home-manager/modules/desktop/quickshell/default.nix
+++ configs/home-manager/modules/desktop/quickshell/default.nix
@@ -1,24 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     quickshell
-    jq
-    socat
-    pamixer
-    brightnessctl
-    wl-clipboard
-    cliphist
-    bluez
-    networkmanager
-    imagemagick
-    qt6.qtmultimedia
-    qt6.qt5compat
-    qt6.qtwebsockets
-    ffmpeg
   ];
 
   programs.quickshell = {
     enable = true;
   };
-
-  xdg.configFile."hypr/scripts".source = ./scripts;
 }
