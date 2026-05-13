5576fe560b936adf4deaf400ecbad57a33224809
midboob <edwarddan72@gmail.com>
2026-05-13 12:28:01 -0700

    feat: ✨ added system specific monitor settings for mangoWC

--- configs/hosts/desktop/home/modules/de/mango/general.nix
+++ configs/hosts/desktop/home/modules/de/mango/general.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.mango.settings = {
     monitorrule = [
-      "name:^.*$,width:2560,height:1440,refresh:180,x:0,y:0,scale:1.5"
+      "name:^*$,width:2560,height:1440,refresh:180,x:0,y:0,scale:1.5"
     ];
   };
 }
--- configs/hosts/g14/home/modules/de/sway/general.nix
+++ configs/hosts/g14/home/modules/de/sway/general.nix
@@ -1,11 +1,11 @@
-{pkgs, ...}: {
+{
   wayland.windowManager.sway.config = {
-      output = {
-        "*" = {
-          mode = "2650x1600@60.000Hz";
-          pos = "0 0";
-          scale = "1.5";
-        };
+    output = {
+      "*" = {
+        mode = "2650x1600@120.000Hz";
+        pos = "0 0";
+        scale = "1.5";
       };
     };
+  };
 }
