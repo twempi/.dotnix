58cfaf83e58bd7b42c6d6c82f55603d8c2f7a99f
midboob <edwarddan72@gmail.com>
2026-05-15 13:51:29 -0700

    feat: ✨ drive changes

--- configs/hosts/desktop/system/modules/drives.nix
+++ configs/hosts/desktop/system/modules/drives.nix
@@ -1,50 +1,42 @@
 {
   # Mounting additional drives
-  fileSystems = {
-    #    "/mnt/Storage" = {
-    #      device = "/dev/disk/by-uuid/9A62A5A562A5871B";
-    #      fsType = "ntfs3";
-    #      options = [
-    #        "rw"
-    #        "uid=1000"
-    #        "gid=100"
-    #        "umask=000"
-    #        "exec"
-    #        "nofail"
-    #        "noatime"
-    #        "x-systemd.automount"
-    #        "windows_names"
-    #      ];
-    #    };
+  fileSystems = let
+    ntfsOptions = [
+      "rw"
+      "uid=1000"
+      "gid=100"
+      "umask=000"
+      "exec"
+      "nofail"
+      "noatime"
+      "x-systemd.automount"
+      "windows_names"
+    ];
+  in {
+    "/mnt/Storage" = {
+      device = "/dev/disk/by-uuid/9A62A5A562A5871B";
+      fsType = "ntfs3";
+      options = ntfsOptions;
+    };
 
     "/mnt/Windows" = {
       device = "/dev/disk/by-uuid/01DC4262B6A13A40";
       fsType = "ntfs3";
-      options = [
-        "rw"
-        "uid=1000"
-        "gid=100"
-        "umask=000"
-        "exec"
-        "nofail"
-        "noatime"
-        "x-systemd.automount"
-        "windows_names"
-      ];
+      options = ntfsOptions;
     };
 
-    #   "/home/edward/Documents/notes" = {
-    #     device = "/mnt/Storage/Documents/notes";
-    #     fsType = "none";
-    #     options = ["bind" "rw"];
-    #     depends = ["/mnt/Storage"];
-    #   };
+    "/home/edward/Documents/notes" = {
+      device = "/mnt/Storage/Documents/notes";
+      fsType = "none";
+      options = ["bind" "rw" "nofail"];
+      depends = ["/mnt/Storage"];
+    };
 
-    #   "/home/edward/Documents/school" = {
-    #     device = "/mnt/Storage/Documents/school";
-    #     fsType = "none";
-    #     options = ["bind" "rw"];
-    #     depends = ["/mnt/Storage"];
-    #   };
+    "/home/edward/Documents/school" = {
+      device = "/mnt/Storage/Documents/school";
+      fsType = "none";
+      options = ["bind" "rw" "nofail"];
+      depends = ["/mnt/Storage"];
+    };
   };
 }
