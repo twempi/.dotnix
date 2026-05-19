6e4441e7753a995df8acfcd96dfdf58bcc3e4f46
midboob <edwarddan72@gmail.com>
2026-05-18 21:40:44 -0700

    feat: ✨ changed stylix theme and added vim mode to fish shell

--- configs/home-manager/modules/cli/fish/default.nix
+++ configs/home-manager/modules/cli/fish/default.nix
@@ -1,88 +1,75 @@
-{ pkgs, ...}: {
+{pkgs, ...}: {
+  stylix.targets.fish.enable = true;
 
-	stylix.targets.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    # enableCompletion = true;
+    # autosuggestion.enable = true;
+    # syntaxHighlighting.enable = true;
 
-	programs.fish = {
-		enable = true;
-		# enableCompletion = true;
-		# autosuggestion.enable = true;
-		# syntaxHighlighting.enable = true;
-
-		shellAliases = {
-
-			# Rebuilds
-			# nrs = "nh os switch";
-			# nrb = "nh os boot";
-			# hms = "nh home switch";
-
-			# nrs = "sudo nixos-rebuild switch --impure --flake ~/.dotnix";
-			# nrb = "sudo nixos-rebuild boot --impure --flake ~/.dotnix";
-
-			# nrs = "sudo nixos-rebuild switch --flake ~/.dotnix";
-			# nrb = "sudo nixos-rebuild boot --flake ~/.dotnix";
-
-			# hms = "home-manager switch --flake ~/.dotnix";
+    shellAliases = {
       syu = "nix flake update --flake ~/.dotnix && nrs && hms";
 
-			# Nvim
-			v = "nvim";
-			vi = "nvim";
-			vim = "nvim";
+      # Nvim
+      v = "nvim";
+      vi = "nvim";
+      vim = "nvim";
 
-			# Tmux
-			t = "tmux";
+      # Tmux
+      t = "tmux";
 
       # Yazi
       y = "yazi";
-			
-			# Eza
-			ls = "eza --icons=auto --classify --group-directories-first --header --time-style=long-iso";
-			la = "eza -la --icons=auto --classify --group-directories-first --header --time-style=long-iso";
-			tree = "eza --tree --icons=auto --classify --group-directories-first --header --time-style=long-iso";
 
-			# Common commands
-			cp = "cp -iv";
-			mv = "mv -iv";
-			rm = "rm -vI";
-			bc = "bc -ql";
-			mkd = "mkdir -pv";
+      # Eza
+      ls = "eza --icons=auto --classify --group-directories-first --header --time-style=long-iso";
+      la = "eza -la --icons=auto --classify --group-directories-first --header --time-style=long-iso";
+      tree = "eza --tree --icons=auto --classify --group-directories-first --header --time-style=long-iso";
 
-			# Git
-			g = "git";
-			gs = "git status";
-			gc = "git commit";
-			ga = "git add";
-			gpl = "git pull";
-			gpom = "git pull origin master";
-			gpu = "git push";
-			gpuom = "git push origin master";
-			gd = "git diff --output-indicator-new=' ' --output-indicator-old=' '";
-			gch = "git checkout";
-			gnb = "git checkout -b";
-			gac = "git add . && git commit";
-			grs = "git restore --staged .";
-			gre = "git restore";
-			gr = "git remote";
-			gcl = "git clone";
-			gl = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold green)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold yellow)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
-			gt = "git ls-tree -r master --name-only";
-			grm = "git remote";
-			gb = "git branch";
-			gf="git fetch";
+      # Common commands
+      cp = "cp -iv";
+      mv = "mv -iv";
+      rm = "rm -vI";
+      bc = "bc -ql";
+      mkd = "mkdir -pv";
 
-			# notes
-			notes = "cd ~/Documents/notes/";
+      # Git
+      g = "git";
+      gs = "git status";
+      gc = "git commit";
+      ga = "git add";
+      gpl = "git pull";
+      gpom = "git pull origin master";
+      gpu = "git push";
+      gpuom = "git push origin master";
+      gd = "git diff --output-indicator-new=' ' --output-indicator-old=' '";
+      gch = "git checkout";
+      gnb = "git checkout -b";
+      gac = "git add . && git commit";
+      grs = "git restore --staged .";
+      gre = "git restore";
+      gr = "git remote";
+      gcl = "git clone";
+      gl = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold green)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold yellow)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+      gt = "git ls-tree -r master --name-only";
+      grm = "git remote";
+      gb = "git branch";
+      gf = "git fetch";
 
-			# ani-cli
-			ani = "ani-cli";
+      # notes
+      notes = "cd ~/Documents/notes/";
 
-			# shut down
-			die = "sudo shutdown now";
-		};
+      # ani-cli
+      ani = "ani-cli";
 
-			interactiveShellInit = ''
-				set fish_greeting
-        fastfetch
-				'';
-	};
+      # shut down
+      die = "sudo shutdown now";
+    };
+
+    interactiveShellInit = ''
+      set fish_greeting
+      fish_vi_key_bindings
+      fastfetch
+    '';
+  };
 }
--- configs/system/modules/theme/stylix.nix
+++ configs/system/modules/theme/stylix.nix
@@ -23,8 +23,8 @@ in {
       # base16Scheme = inputs.tt-schemes + "/base16/gruvbox-material-dark-medium.yaml";
       # base16Scheme = inputs.tt-schemes + "/base16/tokyo-night-moon.yaml";
       # base16Scheme = inputs.tt-schemes + "/base16/ocean.yaml";
-      # base16Scheme = inputs.tt-schemes + "/base16/black-metal-dark-funeral.yaml";
-      base16Scheme = inputs.tt-schemes + "/base16/grayscale-dark.yaml";
+      base16Scheme = inputs.tt-schemes + "/base16/black-metal-dark-funeral.yaml";
+      # base16Scheme = inputs.tt-schemes + "/base16/grayscale-dark.yaml";
 
       polarity = "dark";
 
