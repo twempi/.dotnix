803cb5d35ec35d87362a26e91a4ea0137cca7030
midboob <edwarddan72@gmail.com>
2026-06-19 18:34:14 -0700

    feat: ✨ ssytem update

--- configs/hosts/desktop/home/modules/windowmanagers/hyprland/windowrules.nix
+++ configs/hosts/desktop/home/modules/windowmanagers/hyprland/windowrules.nix
@@ -1,6 +1,14 @@
 {
-  wayland.windowManager.hyprland.workspace = [
-    "1, monitor:DP-2, default:true"
-    "2, monitor:HDMI-A-1, default:true"
+  wayland.windowManager.hyprland.settings.workspace_rule = [
+    {
+      workspace = "1";
+      monitor = "DP-2";
+      default = true;
+    }
+    {
+      workspace = "2";
+      monitor = "HDMI-A-1";
+      default = true;
+    }
   ];
 }
--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v5";
+      url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
