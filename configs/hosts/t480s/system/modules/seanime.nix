a5e974f9549b5784e89e7caa7f2400c5c20b2d0a
midboob <edwarddan72@gmail.com>
2026-06-20 19:25:37 -0700

    feat: ✨ some changeas

--- configs/hosts/t480s/system/modules/seanime.nix
+++ configs/hosts/t480s/system/modules/seanime.nix
@@ -1,10 +1,33 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     seanime
-    mpv
     ffmpeg
-    qbittorrent
+    mpv
+    qbittorrent-nox
   ];
 
-  networking.firewall.allowedTCPPorts = [43211];
+  networking.firewall.allowedTCPPorts = [
+    43211
+    8080
+  ];
+
+  systemd.services.seanime = {
+    description = "Seanime media server";
+    wantedBy = ["multi-user.target"];
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
+
+    serviceConfig = {
+      User = "YOUR_USERNAME";
+      WorkingDirectory = "/home/YOUR_USERNAME";
+      ExecStart = "${pkgs.seanime}/bin/seanime";
+      Restart = "on-failure";
+      RestartSec = 5;
+
+      Environment = [
+        "SEANIME_SERVER_HOST=0.0.0.0"
+        "SEANIME_SERVER_PORT=43211"
+      ];
+    };
+  };
 }
