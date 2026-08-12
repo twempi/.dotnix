9e295a404bb86d41f4c69cbc9d06c0dad93004e6
midboob <edwarddan72@gmail.com>
2026-08-12 13:03:21 -0700

    feat: ✨ some pihole changes

--- configs/hosts/t480s/system/modules/pihole.nix
+++ configs/hosts/t480s/system/modules/pihole.nix
@@ -1,105 +1,31 @@
-{pkgs, ...}: let
-  webPort = 8081;
-in {
+{ ... }: {
   services.pihole-ftl = {
     enable = true;
-
-    # We handle the firewall ourselves below.
-    openFirewallDNS = false;
-    openFirewallDHCP = false;
-    openFirewallWebserver = false;
-
-    privacyLevel = 0;
 
     lists = [
       {
         url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
-        description = "StevenBlack unified hosts list";
       }
     ];
 
-    queryLogDeleter = {
-      enable = true;
-      age = 30;
-      interval = "weekly";
-    };
+    settings.dns = {
+      upstreams = [
+        "1.1.1.1"
+        "1.0.0.1"
+      ];
 
-    settings = {
-      dns = {
-        upstreams = [
-          "1.1.1.1"
-          "1.0.0.1"
-        ];
-
-        interface = "enp0s31f6";
-        listeningMode = "SINGLE";
-      };
-
-      dhcp.active = false;
-
-      # Prevent changing the Nix-managed config from the UI.
-      misc.readOnly = true;
-
-      webserver.api = {
-        cli_pw = true;
-        pwhash = "";
-      };
+      interface = "enp0s31f6";
+      listeningMode = "SINGLE";
     };
   };
 
   services.pihole-web = {
     enable = true;
-
-    hostName = "pihole.tailae03d0.ts.net";
-
-    # Only accessible locally.
-    # Tailscale Serve proxies to this.
-    ports = [
-      "127.0.0.1:${toString webPort}"
-    ];
+    ports = [ "127.0.0.1:8081" ];
   };
 
-  # DNS accessible only through the LAN NIC.
   networking.firewall.interfaces.enp0s31f6 = {
-    allowedTCPPorts = [53];
-    allowedUDPPorts = [53];
-  };
-
-  systemd.services.tailscale-serve-pihole = {
-    description = "Expose Pi-hole as a Tailscale Service";
-
-    after = [
-      "pihole-ftl.service"
-      "tailscaled.service"
-    ];
-
-    wants = [
-      "pihole-ftl.service"
-      "tailscaled.service"
-    ];
-
-    wantedBy = ["multi-user.target"];
-
-    restartIfChanged = true;
-
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      TimeoutStartSec = 30;
-
-      ExecStop =
-        "${pkgs.util-linux}/bin/flock -w 120 /run/tailscale-serve.lock "
-        + "${pkgs.tailscale}/bin/tailscale serve clear svc:pihole";
-    };
-
-    script = ''
-      set -euo pipefail
-
-      ${pkgs.util-linux}/bin/flock -w 120 /run/tailscale-serve.lock \
-        ${pkgs.tailscale}/bin/tailscale serve --yes \
-          --service=svc:pihole \
-          --https=443 \
-          http://127.0.0.1:${toString webPort}
-    '';
+    allowedTCPPorts = [ 53 ];
+    allowedUDPPorts = [ 53 ];
   };
 }
