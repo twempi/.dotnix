0522fcff060ff7dcc2f4d6a031c6ea147e83eff2
midboob <edwarddan72@gmail.com>
2026-08-16 23:44:15 -0700

    feat: ✨ changed stylix theme to rosepine

--- configs/system/modules/theme/stylix.nix
+++ configs/system/modules/theme/stylix.nix
@@ -17,10 +17,10 @@ in {
       enable = true;
       autoEnable = false;
 
-      # base16Scheme = inputs.tt-schemes + "/base16/rose-pine.yaml";
+      base16Scheme = inputs.tt-schemes + "/base16/rose-pine.yaml";
       # base16Scheme = inputs.tt-schemes + "/base16/everforest-dark-medium.yaml";
       # base16Scheme = inputs.tt-schemes + "/base16/catppuccin-mocha.yaml";
-      base16Scheme = inputs.tt-schemes + "/base16/gruvbox-material-dark-medium.yaml";
+      # base16Scheme = inputs.tt-schemes + "/base16/gruvbox-material-dark-medium.yaml";
       # base16Scheme = inputs.tt-schemes + "/base16/tokyo-night-moon.yaml";
       # base16Scheme = inputs.tt-schemes + "/base16/ocean.yaml";
       # base16Scheme = inputs.tt-schemes + "/base16/black-metal-dark-funeral.yaml";
